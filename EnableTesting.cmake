47bb66ecd482f6259bcfc0ba1525291767ea154b
Vladyslav Tsymbal <vladyslav.tsymbal@gmail.com>
2025-04-05 15:07:54 +0300

    Fix link error in unit tests

--- EnableTesting.cmake
+++ EnableTesting.cmake
@@ -5,23 +5,25 @@ file (GLOB_RECURSE _TEST_SRCS ${TEST_DIR}/*.cpp)
 set (TEST_SRCS ${_TEST_SRCS}
                src/http/HttpClient.cpp
                src/http/HttpRequest.cpp
+               src/http/HttpResponse.cpp
                src/http/HttpConnection.cpp
                src/http/HttpConnectionFactory.cpp
                src/utils/network/NetworkUtils.cpp
                src/utils/network/AddrInfoBuilder.cpp)
 
 add_executable (
-    rss-reader-tests
+    rss_reader_tests
     ${TEST_SRCS}
 )
 
-target_include_directories (rss-reader-tests PRIVATE include)
-target_include_directories (rss-reader-tests PRIVATE tests/include)
+target_include_directories (rss_reader_tests PRIVATE include)
+target_include_directories (rss_reader_tests PRIVATE tests/include)
 target_link_libraries (
-    rss-reader-tests
+    rss_reader_tests
     GTest::gmock_main
     spdlog
+    Boost::algorithm
 )
 
 include (GoogleTest)
-gtest_discover_tests (rss-reader-tests)
+gtest_discover_tests (rss_reader_tests)
