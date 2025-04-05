268b5999d5439989792713a65117f50f6ce7e172
Vladyslav Tsymbal <vladyslav.tsymbal@gmail.com>
2025-04-05 14:57:08 +0300

    Cleanup boost.cmake

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -4,7 +4,6 @@ set(BOOST_ENABLE_CMAKE ON)
 FetchContent_Declare(
     Boost
     GIT_REPOSITORY https://github.com/boostorg/boost.git
-    # GIT_REPOSITORY https://github.com/boostorg/cmake.git
     GIT_TAG boost-1.85.0
     GIT_SHALLOW TRUE
 )
@@ -16,5 +15,4 @@ else()
     message(STATUS "Boost is already fetched and available.")
 endif()
 
-#target_link_libraries (rss_reader PRIVATE Boost::algorithm)
 list(APPEND libs_to_link "Boost::algorithm")
